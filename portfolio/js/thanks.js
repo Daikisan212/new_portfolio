0f62f7c3d75f90377197a38fc851c7d15cf20842
横山大希 <70083780+Daikisan212@users.noreply.github.com>
2021-04-02 17:49:15 +0900

    Delete

--- portfolio/js/thanks.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.burger-btn').on('click',function(){
-        $('.burger-btn').toggleClass('close');
-        $('.nav-wrapper').fadeToggle(500);
-        $('body').toggleClass('noscroll');
-    });
-    window.scrollTo(0,document.body.scrollHeight);
-});
